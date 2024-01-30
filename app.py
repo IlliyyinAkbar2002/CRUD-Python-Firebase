bd776317a76b2a78fb481c285ff772a7fca4c71e
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2024-01-30 21:33:39 +0700

    Simple project of CRUD-python with firebase.

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ def create():
         "name": name,
         "age": age
     }
-    db.child("users").push(data)
+    db.collection("default").add(data)
     print("Data created")
 
 
 def read():
     print("Read data")
-    users = db.child("users").get()
-    for user in users:
+    default = db.collection("default").get()
+    for user in default:
         print(user.id, user.to_dict())
 
 
@@ -39,37 +39,42 @@ def update():
         "name": name,
         "age": age
     }
-    db.child("users").update(data)
+    db.collection("default").document(name).update(data)
     print("Data updated")
 
 
 def delete():
     print("Delete data")
-    db.child("users").remove()
+    name = input("Name: ")
+    db.collection("default").document(name).delete()
     print("Data deleted")
 
 
 def main():
-    print("Crud python with Firebase")
-    print("1. Create data")
-    print("2. Read data")
-    print("3. Update data")
-    print("4. Delete data")
+    while True:
+        print("Crud python with Firebase")
+        print("1. Create data")
+        print("2. Read data")
+        print("3. Update data")
+        print("4. Delete data")
+        print("5. Exit")
 
-    input("Press Enter to continue...")
+        input("Press Enter to continue...")
 
-    option = input("Select an option: ")
+        option = input("Select an option: ")
 
-    if option == "1":
-        create()
-    elif option == "2":
-        read()
-    elif option == "3":
-        update()
-    elif option == "4":
-        delete()
-    else:
-        print("Invalid option")
+        if option == "1":
+            create()
+        elif option == "2":
+            read()
+        elif option == "3":
+            update()
+        elif option == "4":
+            delete()
+        elif option == "5":
+            exit()
+        else:
+            print("Invalid option")
 
 
 if __name__ == "__main__":
